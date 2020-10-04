69c5fcc9eaf082c2f7bdf5cf5729bf4323e50b19
l37133582 <37133582@qq.com>
2020-10-04 23:25:06 +0800

    组件通信：v-model 更新

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
   <div id="app">
-    <div>VUE知识点梳理</div>
-    <div>每个分支都是一个知识点</div>
+    <v-model v-model="inputValue"/>
   </div>
 </template>
 
 <script>
+import VModel from '@/components/VModel'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {VModel},
+  data(){
+    return{
+      inputValue: '123'
+    }
+  },
+  watch: {
+    inputValue(newVal, oldVal){
+      console.log('watch newVal', newVal, 'watch oldVal', oldVal);
+    }
+  }
 }
 </script>
 
@@ -16,12 +27,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #fff;
-  text-align: center;
-  min-height: 100px;
-  background-color: #2c3e50;
-  margin: 100px auto;
-  font-size: 40px;
-  line-height: 100px;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
---
+++ src/components/VModel.vue
@@ -0,0 +1,68 @@
+<template>
+  <section>
+    <header>
+      <p>父组件通过v-model传递值给子组件时，会自动传递一个value的prop属性，在子组件中通过this.$emit(‘input',val)自动修改v-model绑定的值</p>
+      <p>有三种写法</p>
+      <p>写法一： value 配合 input事件</p>
+      <p>写法二： model自定义value和事件名</p>
+      <p>写法二： $attrs 配合 input事件</p>
+    </header>
+    <article>
+      <label for="input">
+        v-model input:
+        <!--        ----------------------------- 写法一 ---------------------------------- -->
+        <!--        <input id="input" type="text" :value="value" @input="$emit('input', $event.target.value)">-->
+        <!--        ----------------------------- 写法二 ---------------------------------- -->
+        <input id="input" type="text" :value="text1" @input="$emit('change1', $event.target.value)"/>
+        <!--        ----------------------------- 写法三 ---------------------------------- -->
+        <!--        <input id="input" type="text" v-bind="$attrs" @input="$emit('input', $event.target.value)">-->
+      </label>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "VModel",
+  // ----------------------- 写法一 -------------------------------------
+  // props: {
+  //   value: {
+  //     type: [Number,String,null],
+  //     require: true,
+  //     default: 0
+  //   }
+  // }
+
+  // ---------------------- 写法二 -------------------------------------
+  model: {
+    prop: 'text1', // 对应 props text1
+    event: 'change1'
+    // 1. 上面的 emit事件名 和 model.event 要对应起来
+    // 2. 上面的 value 和 model.prop 要对应起来
+  },
+  props: {
+    text1: {
+      type: [Number, String, null],
+      require: true,
+      default() {
+        return ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+header {
+  background-color: #2c3e50;
+  color: aliceblue;
+  padding: 20px;
+}
+
+article {
+  background-color: blanchedalmond;
+  margin-top: 20px;
+  min-height: 200px;
+  padding: 20px;
+}
+</style>
