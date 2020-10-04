ee726213d1d109f540bf5a2718b43538226feba9
l37133582 <37133582@qq.com>
2020-10-04 17:10:27 +0800

    改变主分支

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,13 @@
 <template>
   <div id="app">
-    <child :num="num" @increase="handleIncrease" @decrease="handleDecrease"/>
+    <div>VUE知识点梳理</div>
+    <div>每个分支都是一个知识点</div>
   </div>
 </template>
 
 <script>
-import Child from '@/components/child'
 export default {
-  name: 'App',
-  components: {Child},
-  data(){
-    return{
-      num: 0
-    }
-  },
-  methods: {
-
-    // 累加
-    handleIncrease(){
-      this.num++
-    },
-
-    // 累减
-    handleDecrease(){
-      this.num--
-    },
-
-  }
+  name: 'App'
 }
 </script>
 
@@ -35,7 +16,12 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fff;
+  text-align: center;
+  min-height: 100px;
+  background-color: #2c3e50;
+  margin: 100px auto;
+  font-size: 40px;
+  line-height: 100px;
 }
 </style>
