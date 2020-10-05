9244f91089e352c5c36c85f08704041c3a8157b7
l37133582 <37133582@qq.com>
2020-10-05 17:41:37 +0800

    vue基础：class和style 更新

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <template>
   <div id="app">
-    <div>VUE知识点梳理</div>
-    <div>每个分支都是一个知识点</div>
+    <class-demo/>
   </div>
 </template>
 
 <script>
+import ClassDemo from "@/components/ClassDemo";
 export default {
-  name: 'App'
+  name: 'App',
+  components: {ClassDemo},
+  data(){
+    return{
+      num: 0
+    }
+  },
+  methods: {
+
+    // 累加
+    handleIncrease(){
+      this.num++
+    },
+
+    // 累减
+    handleDecrease(){
+      this.num--
+    },
+
+  }
 }
 </script>
 
@@ -16,12 +35,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #fff;
-  text-align: center;
-  min-height: 100px;
-  background-color: #2c3e50;
-  margin: 100px auto;
-  font-size: 40px;
-  line-height: 100px;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
---
+++ src/components/ClassDemo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <p :class="{ black: isBlack, yellow: isYellow }">对象形式使用 class</p>
+    <p :class="isBlack ? black : yellow">三元表达式形式使用 class</p>
+    <p :class="[black, yellow]">数组形式使用 class</p>
+    <p :style="styleData">对象形式使用 style</p>
+    <p :style="{backgroundColor: isYellow ? 'yellow': '#5c5c5c'}">三元表达式使用 style</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isBlack: true,
+      isYellow: true,
+
+      black: 'black',
+      yellow: 'yellow',
+
+      styleData: {
+        fontSize: '40px', // 转换为驼峰式
+        color: 'red',
+        backgroundColor: '#ccc' // 转换为驼峰式
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.black {
+  background-color: #999;
+}
+
+.yellow {
+  color: yellow;
+}
+</style>
